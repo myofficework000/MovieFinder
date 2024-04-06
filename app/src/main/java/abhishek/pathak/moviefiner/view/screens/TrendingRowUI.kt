9cb92ac5708e2877669052c2636debdc9b9aed7e
DHNGL-ABHISHEKP\Abhishek P <myofficework000@gmail.com>
2024-04-07 01:49:26 +0530

    fixed error

--- app/src/main/java/abhishek/pathak/moviefiner/view/screens/TrendingRowUI.kt
+++ app/src/main/java/abhishek/pathak/moviefiner/view/screens/TrendingRowUI.kt
@@ -1,45 +1,28 @@
-<<<<<<< HEAD
 package abhishek.pathak.moviefiner.view.screens
 
 import abhishek.pathak.moviefiner.R
-import abhishek.pathak.moviefiner.model.data.Movie
-import abhishek.pathak.moviefiner.navigation.NavigationItem
 import abhishek.pathak.moviefiner.ui.theme.LightGray20
 import abhishek.pathak.moviefiner.ui.theme.dp_16
-import abhishek.pathak.moviefiner.ui.theme.dp_20
-import abhishek.pathak.moviefiner.ui.theme.dp_4
 import abhishek.pathak.moviefiner.ui.theme.dp_8
-import abhishek.pathak.moviefiner.ui.theme.sp_10
 import abhishek.pathak.moviefiner.ui.theme.sp_20
 import abhishek.pathak.moviefiner.ui.theme.sp_30
 import abhishek.pathak.moviefiner.viewmodel.HiltMovieViewModel
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 
 @Preview
 @Composable
@@ -53,43 +36,48 @@ fun TrendingRowUI(movieViewModel: HiltMovieViewModel = hiltViewModel()) {
     movieViewModel.getTrendingMovies()
     val responseMovie = movieViewModel.trendingMovie.observeAsState()
 
-    ConstraintLayout (
+    ConstraintLayout(
         Modifier
             .fillMaxWidth()
-            .padding(dp_8, dp_16)){
+            .padding(dp_8, dp_16)
+    ) {
         val (title, btnViewAll) = createRefs()
         val (lazyRowTrendMovie) = createRefs()
 
         Text(text = "Trending",
             fontSize = sp_30,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.constrainAs(title){
+            modifier = Modifier.constrainAs(title) {
                 top.linkTo(parent.top)
                 start.linkTo(parent.start)
             })
 
         TextButton(onClick = { /*TODO*/ },
-            modifier = Modifier.constrainAs(btnViewAll){
+            modifier = Modifier.constrainAs(btnViewAll) {
                 top.linkTo(parent.top)
                 end.linkTo(parent.end)
             }) {
 
-            Text(text = "View all",
+            Text(
+                text = "View all",
                 fontSize = sp_20,
-                color = LightGray20)
+                color = LightGray20
+            )
 
-            Image(painter = painterResource(id = R.drawable.baseline_keyboard_double_arrow_right_24),
-                contentDescription = null)
+            Image(
+                painter = painterResource(id = R.drawable.baseline_keyboard_double_arrow_right_24),
+                contentDescription = null
+            )
         }
 
-        LazyRow(modifier = Modifier.constrainAs(lazyRowTrendMovie){
+        LazyRow(modifier = Modifier.constrainAs(lazyRowTrendMovie) {
             start.linkTo(parent.start)
             top.linkTo(title.bottom)
         }) {
 
             responseMovie.value?.let { movieList ->
                 movieList.movies?.let { movies ->
-                    items(movies){
+                    items(movies) {
                         MovieItemUI(movie = it, rememberNavController())
                     }
                 }
@@ -98,7 +86,6 @@ fun TrendingRowUI(movieViewModel: HiltMovieViewModel = hiltViewModel()) {
 //            if(movies.value != null){
 //                items(movies.value!!.movies){
 //                    MovieItemUI(it, rememberNavController())
-=======
 //package abhishek.pathak.moviefiner.view.screens
 //
 //import abhishek.pathak.moviefiner.R
@@ -191,7 +178,7 @@ fun TrendingRowUI(movieViewModel: HiltMovieViewModel = hiltViewModel()) {
 //                    items(movies){
 //                        MovieItemUI(movie = it, rememberNavController())
 //                    }
->>>>>>> 810bd4a702df089b31471899b11d83aaa3046200
+            >>>>>>> 810bd4a702df089b31471899b11d83aaa3046200
 //                }
 //            }
 //
