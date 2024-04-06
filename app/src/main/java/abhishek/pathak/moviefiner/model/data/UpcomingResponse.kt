841a922387534afea7fc913c1c9b20055b732520
DHNGL-ABHISHEKP\Abhishek P <myofficework000@gmail.com>
2024-04-07 01:45:55 +0530

    fixed conflicts

--- app/src/main/java/abhishek/pathak/moviefiner/model/data/UpcomingResponse.kt
+++ app/src/main/java/abhishek/pathak/moviefiner/model/data/UpcomingResponse.kt
@@ -1,9 +1,19 @@
 package abhishek.pathak.moviefiner.model.data
 
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Data class representing the response for upcoming movies.
+ * @property dates The date range for the movies.
+ * @property page The page number of the response.
+ * @property results The list of upcoming movies.
+ * @property totalPages The total number of pages available.
+ * @property totalResults The total number of movies available.
+ */
 data class UpcomingResponse(
-    val dates: DateRange,
-    val page: Int,
-    val results: List<Movie>,
-    val total_pages: Int,
-    val total_results: Int
+    @SerializedName("dates") val dates: DateRange,
+    @SerializedName("page") val page: Int,
+    @SerializedName("results") val results: List<Movie>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
 )
